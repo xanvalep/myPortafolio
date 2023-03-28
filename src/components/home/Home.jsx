a8ce3f10d23bb1f99253e068c0419d24fa74bf0f
Le-vagabondev <75924500+Le-vagabondev@users.noreply.github.com>
2023-03-28 02:11:00 -0500

    alpha-gsap

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,28 @@
 import '..//..//css/Home.scss';
 import logR from '..//..//svgs/logR.svg';
 import Typskill from './Typskill';
-
-
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect,useRef } from 'react';
+gsap.registerPlugin(ScrollTrigger);
 export default function Home (props ) {
- 
+  let imgLog = useRef(null);
+
+  useEffect(() => {
+      console.log(imgLog);
+      const tl = gsap.timeline({repeat: 0, repeatDelay: 1});
+      tl.to(imgLog, {opacity: 0, duration: 1});
+      tl.to(imgLog, {opacity: 1, duration: 1});
+  },[])
+
 return(
     <div className="container my-3"> 
      <section id="hero">
       <div className="hero-container">
       <h1 >
-   
-      <img className="animate__animated animate__fadeIn" src={logR} alt="" srcset="" />
+      <img 
+     ref={e => { imgLog = e}}
+     src={logR} alt="" srcset="" />
       </h1>
       
       <Typskill/>  
